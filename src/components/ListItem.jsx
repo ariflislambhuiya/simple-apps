94b75e6bcd7e45591d1adf49cb00e56af4c05b95
Ariful Islam <Ariful Islam>
2024-02-19 00:49:29 +0600

    first commit

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,28 @@
+const ListItem = ({
+  id,
+  productName,
+  price,
+  stock,
+  quantity,
+  total,
+  increment,
+  decrement,
+}) => (
+  <tr>
+    <th>{productName}</th>
+    <th>{price}</th>
+    <th>{stock}</th>
+    <th>{quantity}</th>
+    <th>{total}</th>
+    <th>
+      <button disabled={quantity === 0} onClick={() => decrement(id)}>
+        -
+      </button>
+      <button disabled={quantity === stock} onClick={() => increment(id)}>
+        +
+      </button>
+    </th>
+  </tr>
+);
+
+export default ListItem;
